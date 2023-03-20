19c75ac5be6e0a3418b74b05872e6e89c4a4780c
RR <ralph.wiersema@hotmail.com>
2023-03-20 13:46:00 +0100

    simpleProgress

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'welcome to the new blog1';
+  const likes = 50; 
+  //output nr's strings & arrays
+  const link = "http://www.google.com";
+
   return (
    <div className="App">
     <div className="content">
-      <h1>App Component</h1>
+      <h1>{title}</h1>
+      <p>liked {likes}</p>
+      <p>{"hello ninjas"}</p>
+      <p>{Math.random() * 10}</p>
+      <a href={link}>website</a>
     </div>
    </div>
   );
 }
+
+
 
 export default App;
